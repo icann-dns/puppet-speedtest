ed35a7b9b99000827469846420aa32b4af0623e5
John Bond <github@johnbond.org>
2017-07-19 17:42:42 +0200

    update params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,27 +1,5 @@
 # speedtest::params
 #
 class speedtest::params {
-  case $::kernel {
-    'FreeBSD': {
-      $use_pip = false
-      $package = 'py27-speedtest-cli'
-    }
-    'Linux': {
-      case $::lsbdistcodename {
-        'trusty': {
-          $use_pip = true
-          $package = 'speedtest-cli'
-        }
-        default: {
-          $use_pip = false
-          $package = 'speedtest-cli'
-        }
-      }
-    }
-    default: {
-      fail("speedtest does not support ${::kernel}")
-    }
-  }
-  $output_dir       = '/var/tmp'
   $output_file_name = "speedtest-${::fqdn}"
 }
